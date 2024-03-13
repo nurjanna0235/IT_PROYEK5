b33a518b2b0a58e58caced76413d7bec1ff856c8
nurjanna0235 <nurjanna@mhs.politala.ac.id>
2024-03-13 17:03:00 +0800

    push pertama it proyek

---
+++ app/Controllers/BeradaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class BeradaController extends BaseController
+{
+    public function index()
+    {
+
+    }
+}
